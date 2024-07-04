a8d0168d4b9158d1cc9c828289a94a6c347fc79f
Robynraps <Robynraps@mail.com>
2024-07-04 20:12:50 +0200

    first commit

---
+++ .vscode/extensions/demystifying-javascript.python-extensions-pack-1.0.3/CHANGELOG.md
@@ -0,0 +1,16 @@
+## 1.0.3
+
+- Removed unavailable packages Ponicode
+- Removed extensions which were already getting provided by Microsoft
+
+## 1.0.2
+
+- Updated description
+
+## 1.0.1
+
+- Updated keywords
+
+## 1.0.0
+
+- Initial release
---
+++ .vscode/extensions/demystifying-javascript.python-extensions-pack-1.0.3/IIT JAMMU WRITTEN EXAM QUIZ.go
@@ -0,0 +1,149 @@
+IIT JAMMU WRITTEN EXAM QUIZ
+In how many ways 25 boys and 25 girls can be seated in a row so that they are positioned one after the other.
+
+Marked Answer :
+25! * 25! * 2
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+A letter is chosen at random from the word “INTELLIPAAT”, what is the probability that it is a vowel?
+
+Marked Answer :
+5/11
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+The ages of two people differ by 12 years. 3 years ago, the older person was 2 times older than the younger one. What is the age of the elder person?
+
+Marked Answer :
+30
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+A and B take 8 days to complete the work, B and C take 12 days to finish the same work. But, A, B and C can finish the task in 6 days. How much time will it take for A and C to finish the work?
+
+Marked Answer :
+12
+
+TOTAL MARKS : 1MARKS OBTAINED  0
+Let us assume a given function score_print ( int x [500]), that takes an integer array ‘x’ corresponding to marks of 500 students ranging between [0,100].  It processes the marks to print an array F which corresponds to the frequency of each mark above 50. What is the best way to create and store the array F by the function?
+
+Marked Answer :
+A dynamically allocated integer array
+
+TOTAL MARKS : 1MARKS OBTAINED  0
+Given code for a function  int foo (int x[ ], int y) and void main( ):
+
+int foo ( int x [ ], int y)
+{ int *pp= x+y;
+return pp[1]
+}
+void main() {
+int X={10, 22, 31, 4, 52, 12, 10, 11, 6, 30}
+int val = foo (X, 5);
+printf (“%d \n”, val);}
+
+If the function foo() is called by  main (). Then what is the value of variable ‘val’ printed by the above code ?
+
+Marked Answer :
+15
+
+TOTAL MARKS : 1MARKS OBTAINED  0
+Given that memory storage size of ‘int’ data type  is 4 bytes, ‘char’ is 1 byte and ‘float’ is 4 bytes. Also given that a generic pointer size is 4 bytes.
+
+void main (){
+char *x;
+int *y;
+float *z;
+printf(“%d, %d, %d”, sizeof(x), sizeof(y), sizeof(z));
+}
+
+Then given the following declarations, what is the output of the following print statements:
+
+Marked Answer :
+4, 4, 4
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+Given the following function ‘func1( int) ’, called on variable ‘delta’
+
+int func1 (int delta){
+if (delta = = 0){
+delta=1}
+else{
+delta=0}
+return delta}
+
+Let ‘delta’ be a n integer variable whose value can only be either 0 or 1. Then what is the overall effect of calling the function  func1(delta) on the variable delta?
+
+Marked Answer :
+delta = 1- delta
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+Given the following C codes that defines a structure datatype ‘my_data’, and functions foo1() and foo2 () defined over the structure variable.
+
+typedef struct {
+char *m1;
+char *m2;
+} mydata;
+
+void foo1( mydata d){
+d.m1 = “U”;
+d.m2 = “V”;
+printf (“%s %s %sn”, d.m1, d.m2);
+return;
+}
+
+void foo2 (mydata * p){
+p -> m1 = “V”;
+p -> m2 = “W”;
+printf(“%s %sn”, p -> a, p -> b);
+return;
+}
+
+Void main(){
+static mydata k = {“A”, “B”};
+printf (“%s %sn”, k.a, k.b);
+foo1(k);
+printf (“%s %sn”, s.a, s.b);
+foo2(&k);}
+
+Then the output values printed by the codes is:
+
+Marked Answer :
+A B U V A B V W
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+Look at the series : 29, 30, 32, 37, 45, .. what will be the next number in the series?
+
+Marked Answer :
+58
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+AGB, BHA, CID, DJC, … what will be the next sequence of letters?
+
+Marked Answer :
+EKF
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+Thomas, who lives in the UK and is 14 years old, has been asking his parents to buy him a car. His parents are not going to buy Thomas a car, but they will buy him a bicycle instead.
+
+Marked Answer :
+Thomas does not have a driving license
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+Suggest the correction to  “As there was no place in the first bogey, he shifted.”
+
+Marked Answer :
+As there was no room in the first bogey, he moved
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+______ is to Pizza, as writer is to ______?
+
+Marked Answer :
+Baker, Book
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+Find the number Y, so that Y * 1250  is a perfect cube.
+
+Marked Answer :
+100
+
+TOTAL MARKS : 1MARKS OBTAINED  1
+Total Marks12 / 15
